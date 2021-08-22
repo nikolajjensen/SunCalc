ff55a7613c168a960a93966ae52b75c323db85d3
Nikolaj B. Jensen <nikko4913@icloud.com>
2021-08-22 20:12:34 +0200

    Added products, and changed version to 5.5.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SunCalc",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
