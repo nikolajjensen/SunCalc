71725821dc5c94a3ffd88e5f05e9b759dee62a8f
Nikolaj B. Jensen <nikko4913@icloud.com>
2021-08-22 20:02:13 +0200

    Removed platforms.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SunCalc",
-    platforms: [
-        .iOS(.v8),
-        .macOS(.v10_10),
-        .tvOS(.v9),
-        .watchOS(.v2)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
